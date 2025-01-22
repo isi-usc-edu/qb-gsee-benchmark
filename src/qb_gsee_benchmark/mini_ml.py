095ab633525596b031a85226bf5a2d3b6c1d08cb
Penuel, John (US) - SAS <John.Penuel@L3Harris.com>
2025-01-22 10:50:11 -0500

    initial methods defined

--- src/qb_gsee_benchmark/mini_ml.py
+++ src/qb_gsee_benchmark/mini_ml.py
@@ -26,24 +26,15 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
 import argparse
 import logging
-import datetime
+
 import os
 import shutil
 import sys, getopt
 import numpy as np
 import sklearn
+from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 import joblib # for saving the model
 import pandas as pd
@@ -52,8 +43,8 @@ from sklearn.metrics import balanced_accuracy_score
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import classification_report, confusion_matrix 
 from sklearn.model_selection import cross_val_score
-import json
-import pprint
+from sklearn.model_selection import GridSearchCV
+
 import matplotlib.pyplot as plt
 from scipy.spatial import ConvexHull
 from sklearn.preprocessing import StandardScaler
@@ -65,6 +56,7 @@ import shap
 
 
 # global parameters
+RNG_SEED = 42
 FEATURES = [
     "max_vertex_degree",
     "min_vertex_degree",
@@ -88,7 +80,12 @@ UNUSED_FEATURES = [
     "number_of_terms"
 ]
 THRESHOLD_FOR_CONFIDENCE_OF_SOLVABILITY = 0.5
-
+LATENT_MODEL_NAME = "NNMF" # only NNMF is supported at this time.
+MODEL_NAME = "SVM" # only SVM is supported at this time.
+HYPOPT_CV = True
+PARAM_GRID = {'C': [0.001, 0.1, 0.5, 1, 10, 50, 100],  
+            'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 
+            'kernel': ['rbf', 'poly', 'linear']} 
 
 
 
@@ -99,11 +96,16 @@ class MiniML:
         self,
         solver_labels_by_task_uuid: pd.DataFrame,
         hamiltonian_features_by_task_uuid: pd.DataFrame,
-        rng_seed: int=42
+        rng_seed: int=RNG_SEED
     ) -> None:
+        """TODO: docstring"""
         
-        random.seed(rng_seed)
-                
+        self.rng_seed = rng_seed
+        random.seed(self.rng_seed)
+        self.latent_model_name = LATENT_MODEL_NAME
+        self.model_name = MODEL_NAME
+        self.hypopt_cv = HYPOPT_CV
+        self.threshold_for_confidence_of_solvability = THRESHOLD_FOR_CONFIDENCE_OF_SOLVABILITY                
         self.solver_labels = pd.merge(
             hamiltonian_features_by_task_uuid,
             solver_labels_by_task_uuid,
@@ -111,39 +113,82 @@ class MiniML:
             how="inner",
             suffixes="_duplicate_column"
         )
-        self.validate_input_labels()
+        self.param_grid = PARAM_GRID
+        self.kfold_num = 5
+
+        # order of operations matters!
+        self.__validate_input_labels()
+        self.__filter_labels()
+        self.__shuffle_labels()
+        self.__remove_zero_variance_columns()
+        self.__scale_data()
+        self.__train_model()
+        self.__evaluate_model()
+        self.__get_projected_data()
+        self.__compute_ratio_of_solved_to_unsolved()
+
         
         
+        # self.model = None
+        # self.f1_score = None 
+        # self.ml_solvability_ratio = None 
+        # self.shap_plot = None
+        # self.solvability_plot = None 
+        
+        
+
+
+
+
+
+
+    def __repr__(self) -> str:
+        return f"""mini ML model for {self.solver_short_name} ({self.solver_uuid}).
+            f1_score: {self.f1_score}.
+            ml_solvability_ratio: {self.ml_solvability_ratio}.  
+        """
+
+
+
+
+
+
+    def __validate_input_labels(self) -> None:
+        """TODO: docstring"""
+        # check for only one solver_uuid
+        # check for only one solver_short_name
+        # other checks... 
+        ## 
+        num_unique_solver_uuids = self.solver_labels["solver_uuid"].nunique()
+        assert num_unique_solver_uuids == 1, \
+            f"Error in solver labels: num_unique_solver_uuids={num_unique_solver_uuids} and should be 1."
+        num_unique_solver_short_names = self.solver_labels["solver_short_name"].nunique()
+        assert num_unique_solver_short_names == 1, \
+            f"Error in solver labels: num_unique_solver_short_names={num_unique_solver_short_names} and should be 1."
+
+        self.solver_uuid = self.solver_labels["solver_uuid"].values[0]
+        self.solver_short_name = self.solver_labels["solver_short_name"].values[0]        
+
+
+        
+    def __filter_labels(self) -> None:
+        """TODO: docstring.
+        """
         self.X = self.solver_labels[:,FEATURES]
         self.Y = self.solver_labels[:,"label"] # column header is `label`
+        
+
+    def __shuffle_labels(self) -> None:
+        """TODO: docstring.
+        """
         row_index_permutation_list = random.shuffle(list(range(len(self.X))))
         self.X = self.X.iloc[row_index_permutation_list,:]
         self.Y = self.Y.iloc[row_index_permutation_list,:]
 
-        
-        self.remove_zero_variance_columns()
-        
-
-        self.latent_model_name = "NNMF"
-        self.model_name = "SVM"
-        self.hypopt_cv = True
-        self.threshold_for_confidence_of_solvability = THRESHOLD_FOR_CONFIDENCE_OF_SOLVABILITY
-        
-        self.train_model()
-        
- 
-        self.model = None
-        self.f1_score = None 
-        self.ml_solvability_ratio = None 
-        # self.shap_plot = None
-        # self.solvability_plot = None 
-        
-        pass
 
 
-
-    def remove_zero_variance_columns(self) -> list:
-        """_summary_
+    def __remove_zero_variance_columns(self) -> list:
+        """TODO: docstring.
 
         Returns:
             list: _description_
@@ -156,46 +201,250 @@ class MiniML:
         return self.zero_variance_columns
 
 
+    def __scale_data(self) -> None:
+        """TODO:docstring
+        """
+        self.X_train = self.X # TODO: ask Rashmi about this.  seems to flip flop.
+        self.Y_train = self.Y # TODO: ask rashmi about this... are we just using all X/Y data for training?
+        self.standard_scaler = StandardScaler()
+        self.X_sc = self.standard_scaler.fit_transform(self.X_train)
 
 
 
 
+    def __train_model(self) -> None:
+        """TODO: docstring"""
+
+        assert self.model_name =="SVM", "Error.  Only SVM is supported at this time."
+        self.model = SVC(
+            random_state=self.rng_seed,
+            class_weight='balanced'
+        ) 
+        self.model.probability = True
+
+        #SVM on centered and scaled data
+        self.X_train = self.X_sc # TODO: ask Rashmi about this.  seems to flip flop.
+
+        if self.hypopt_cv: # TODO: maybe break this into a separate method.
+            self.model = GridSearchCV(
+                estimator=self.model, # TODO: ask Rashmi about this.  modifying model?
+                param_grid=self.param_grid,
+                cv=self.kfold_num,
+                n_jobs=-1,
+                verbose=2
+            )
+                
+        self.model.fit(
+            self.X_train,
+            self.Y_train
+        )
+    
+
+
+    def __evaluate_model(self) -> np.ndarray:
+        """TODO: docstring.  original:  This function returns the accuracy by the trained ML model ("model" with "model_name") on test_features with test_labels.
+            Returns the f1-score (harmonic mean of precision and recall)
+        """
+        Y_predicted = self.model.predict(self.X_train)
+        output = precision_recall_fscore_support(
+            self.X_train,
+            Y_predicted,
+            labels=np.array([0,1]) # results in this order.
+        )
+        self.precision = output[0]
+        self.recall = output[1]
+        self.f1_score = output[2]
+        self.support = output[3]
+
+        self.precision_interpretation = f"Precision [class (target=False) , class (target=true) ]:  {100*self.precision[0]:0.2f}%,  {100*self.precision[1]::0.2f}"
+        self.recall_interpretation = f"Recall [class (target=False) , class (target=true) ]:  {self.recall[0]:0.2f}%,  {self.recall[1]:0.2f}%"
+        self.f1_score_interpretation = f"F1-score [class (target=False) , class (target=true) ]:  {self.f1_score[0]:0.2f}%,  {self.f1_score[1]:0.2f}%"
+        
+        self.classification_report = classification_report(
+            self.X_train,
+            Y_predicted
+        )
+
+        return self.f1_score
+    
+
+    def __get_projected_data(self) -> None:
+        """TODO: docstring"""
+        assert self.latent_model_name == "NNMF", \
+            "Error: only NNMF is supported at this time."
+
+        # Apply NNMF
+        # Normalize data (NNMF requires non-negative input.  The data is non-negative, but we will scale in the range of min-max of the features which is great for reconstruction of valid points)
+        self.scaler_minmax = MinMaxScaler()
+        self.X_scaled = self.scaler_minmax.fit_transform(self.X)
+        self.nnmf = NMF(
+            n_components=2,
+            init='random',
+            random_state=self.rng_seed,
+            max_iter = 500
+        )
+        self.nnmf_projected_data = self.nnmf.fit_transform(self.X_scaled)
+        
+        # TODO: store components as attributes.  print the coefficients of the first two axes.
+        print('NNMF components:')
+        self.H = self.nnmf.components_
+        print(self.H)
+        
+        fig = plt.figure()
+        plt.title(f"{self.solver_short_name} ({self.solver_uuid[0:4]}...)\nNNMF Components")
+        plt.plot(self.H[0,:],'r-o')
+        plt.plot(self.H[1,:],'g-o')
+        plt.legend(['Component 1', 'Component 2'])
+        plt.xticks(np.arange(0,len(self.X.columns)))
+        plt.xticks(rotation=30)
+        plt.gca().xaxis.set_ticklabels(
+            self.X.columns.to_list(),
+            ha='right'
+        )
+        plt.tight_layout()
+        self.nnmf_figure = fig
+
+        self.reconstruction_error = np.sqrt(
+            np.sum(
+                (self.scaler_minmax.inverse_transform(self.nnmf.inverse_transform(self.nnmf_projected_data)) - self.X)**2
+            )
+        )
 
 
 
-
-
-    def validate_input_labels(self) -> None:
-        # check for only one solver_uuid
-        # check for only one solver_short_name
-        # other checks... 
-        # raise error if found in input `solver_labels` dataframe.
-        ## 
-        # self.solver_uuid = UPDATEME
-        # self.solver_short_name = UPDATEME
-        pass
-
-    def __repr__(self) -> str:
-        return f"""mini ML model for {self.solver_short_name} ({self.solver_uuid}).
-            f1_score: {self.f1_score}.
-            ml_solvability_ratio: {self.ml_solvability_ratio}.  
+    def __compute_ratio_of_solved_to_unsolved(self) -> float:
+        """
+        TODO: docstring.  old one:  X here is the raw data.  It is uncentered and unscaled.  
+        
+        must have run self.__get_projected_data() first.
+        
         """
 
 
-    def train_model(self):
+        # latent_sc, latent_model, proj_data, recons_error = getProjectedData(X, latent_model_name, draw_plot) #just PCA or NNMF in this code.  The UI has more dimensionality reduction algms
+        # self.scaler_minmax, self.nnmf, self.nnmf_projected_data, self.reconstruction_error
+        #  recons_error = getProjectedData(X, latent_model_name, draw_plot) #just PCA or NNMF in this code.  The UI has more dimensionality reduction algms
+
+
+        
+        # min and max in 2 dimensions of projected data
+        xminmax = np.arange(
+            np.min(self.nnmf_projected_data[:, 0]),
+            np.max(self.nnmf_projected_data[:, 0]),
+            0.1
+        )
+        yminmax = np.arange(
+            np.min(self.nnmf_projected_data[:, 1]),
+            np.max(self.nnmf_projected_data[:, 1]),
+            0.1
+        )
+
+        x = np.linspace(xminmax[0], xminmax[-1]+0.09,100)
+        y = np.linspace(yminmax[0], yminmax[-1]+0.09,100)
+        XX, YY = np.meshgrid(x, y)   
+
+        newX = np.c_[XX.ravel(), YY.ravel()] #grid of projected data
+
+        # undo latent transformation
+        orig_dim_data = self.nnmf.inverse_transform(newX) #back to the original dimensionality undoing the rotation, centering, scaling and projection
+        # undo the scaling.
+        orig_dim_data = self.scaler_minmax.inverse_transform(orig_dim_data) #undo scaling
+
+        # SVM model was trained on centered and scaled data on the stats of X, so need to re-do that
+        orig_dim_data_for_pred = self.standard_scaler.transform(orig_dim_data) #(orig_dim_data-scaler.mean_)/np.sqrt(scaler.var_)
+        orig_probs = self.model.predict_proba(np.asarray(orig_dim_data_for_pred))
+        
+        Z0 = orig_probs[:,1].reshape(XX.shape)
+        
+        
+        # plot figure with training data, generated points
+        colors = Z0.flatten()
+        fig = plt.figure()
+        cmap = plt.cm.bwr_r
+        norm = plt.Normalize(0,1)
+        #norm = plt.Normalize(np.min(colors),np.max(colors)) #normalized according to the probabilities in the decision space
+        plt.scatter(
+            x=XX.flatten(),
+            y=YY.flatten(),
+            c=colors,
+            cmap=cmap,
+            norm=norm
+        )
+        #projected training data
+        plt.scatter(
+            x=self.nnmf_projected_data[:,0],
+            y=self.nnmf_projected_data[:,1],
+            c=self.Y,
+            s=50,
+            edgecolors='black',
+            cmap=cmap,
+            norm=norm
+        )
+        cbar = plt.colorbar()
+        cbar.set_label("Probability that solver can estimate GSE (label==True)",rotation=270,x=1.25)
+        plt.title(f"Solver {self.solver_short_name} ({self.solver_uuid[0:4]}...)\nEmbedding: {self.latent_model_name})")
+        self.solvability_surface_plot = fig
+        
+        '''
+        TODO: ask Rashmi:  not doing this.  # Select the top 5 most important features
+        selector = SelectFromModel(rf, max_features=5, prefit=True)
+        X_train_selected = selector.transform(X_train)
+        '''
+        # TODO: ask rashmi:  not computing convex hull.  
+            
+
+        result = np.where(orig_probs[:,1] > self.threshold_for_confidence_of_solvability)
+        self.ml_solvability_ratio = len(result[0])/len(orig_probs[:,1])
+        return self.ml_solvability_ratio
+
+
+
+
+
+
+    def __run_shap(self):
+        """TODO:docstring
+        """
         pass
 
-    def evaluate_model(self):
+    def write_all_plots(self) -> None:
+        """TODO: docstring
+        """
         pass
 
-    def run_shap(self):
+
+    def write_shap_plot_to_png_file(self, output_file_name: str) -> None:
+        """TODO:docstring
+        """
         pass
 
-    def write_shap_plot(self, output_file_name: str):
+    def write_solvability_plot_to_png_file(self, output_file_name: str) -> None:
+        """TODO:docstring
+        """
         pass
 
-    def write_solvability_plot(self, output_file_name: str):
-        pass
+    def write_probs_to_file(self, output_file_name: str) -> None:
+        """_summary_
+
+        Args:
+            output_file_name (str): _description_
+        """
+        Y_predicted = self.model.predict(self.X_train)
+        self.probs = self.model.predict_proba(self.X_train)
+
+        df = pd.DataFrame(
+            {
+                self.X.columns[0]: self.X[self.X.columns[0]],
+                self.X.columns[1]: self.X[self.X.columns[1]],
+                "Y_train": self.Y_train,
+                "Y_predicted": Y_predicted,
+                "prob_class_0": self.probs[:,0],
+                "prob_class_1": self.probs[:,1]
+            }
+        )
+        df.to_csv(output_file_name, index=False)
+    
+    
 
 
 
