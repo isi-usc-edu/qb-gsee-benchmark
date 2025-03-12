5e0565b4ac5fa5030c0ed4be14a9f6d0420afc39
Penuel, John (US) - SAS <John.Penuel@L3Harris.com>
2025-03-12 11:22:02 -0400

    cleaned old PREs.  calculated PREs.

--- scripts/compute_all_PREs_for_all_PRE_configs.sh
+++ scripts/compute_all_PREs_for_all_PRE_configs.sh
@@ -5,11 +5,11 @@
 
 
 
-# mkdir PRE_solution_files_20250224_0.000001_0.1us/
-# mkdir PRE_solution_files_20250224_0.000001_1us/
-# mkdir PRE_solution_files_20250224_0.0001_0.1us/
-# mkdir PRE_solution_files_20250224_0.0001_1us/
-# mkdir PRE_solution_files_20250224_0.0001_1us_parallel/
+mkdir PRE_solution_files_20250310_0.000001_0.1us/
+mkdir PRE_solution_files_20250310_0.000001_1us/
+mkdir PRE_solution_files_20250310_0.0001_0.1us/
+mkdir PRE_solution_files_20250310_0.0001_1us/
+mkdir PRE_solution_files_20250310_0.0001_1us_parallel/
 
 
 # PRE_config_0.000001_0.1us.json
@@ -20,10 +20,10 @@
 
 
 
-./compute_all_PREs_script.py -i LRE_solution_files_before_PRE_20250224 -o PRE_solution_files_20250224_0.000001_0.1us --PRE_config_file PRE_config_0.000001_0.1us.json
-./compute_all_PREs_script.py -i LRE_solution_files_before_PRE_20250224 -o PRE_solution_files_20250224_0.000001_1us --PRE_config_file PRE_config_0.000001_1us.json
-./compute_all_PREs_script.py -i LRE_solution_files_before_PRE_20250224 -o PRE_solution_files_20250224_0.0001_0.1us --PRE_config_file PRE_config_0.0001_0.1us.json
-./compute_all_PREs_script.py -i LRE_solution_files_before_PRE_20250224 -o PRE_solution_files_20250224_0.0001_1us --PRE_config_file PRE_config_0.0001_1us.json
-./compute_all_PREs_script.py -i LRE_solution_files_before_PRE_20250224 -o PRE_solution_files_20250224_0.0001_1us_parallel --PRE_config_file PRE_config_0.0001_1us_parallel.json
+./compute_all_PREs_script.py -i LRE_solution_files_before_PRE_20250310 -o PRE_solution_files_20250310_0.000001_0.1us --PRE_config_file PRE_config_0.000001_0.1us.json
+./compute_all_PREs_script.py -i LRE_solution_files_before_PRE_20250310 -o PRE_solution_files_20250310_0.000001_1us --PRE_config_file PRE_config_0.000001_1us.json
+./compute_all_PREs_script.py -i LRE_solution_files_before_PRE_20250310 -o PRE_solution_files_20250310_0.0001_0.1us --PRE_config_file PRE_config_0.0001_0.1us.json
+./compute_all_PREs_script.py -i LRE_solution_files_before_PRE_20250310 -o PRE_solution_files_20250310_0.0001_1us --PRE_config_file PRE_config_0.0001_1us.json
+./compute_all_PREs_script.py -i LRE_solution_files_before_PRE_20250310 -o PRE_solution_files_20250310_0.0001_1us_parallel --PRE_config_file PRE_config_0.0001_1us_parallel.json
 
 
